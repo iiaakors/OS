f22f45291e7eab60e207260978b63456feeba8ca
iiaakors <iiaakors@gmail.com>
2017-02-21 01:25:29 +0200

    OS Test

---
+++ SimpleSolution.Tests/SimpleSolutionTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleSolution.Tests
+{
+	[TestClass]
+	public class SimpleSolutionTests
+	{
+		private const string testData = @"FirstName,LastName,Address,PhoneNumber\n
+										Jimmy,Smith,102 Long Lane,29384857\n
+										Clive,Owen,65 Ambling Way,31214788\n
+										James,Brown,82 Stewart St,32114566\n
+										Graham,Howe,12 Howard St,8766556\n
+										John,Howe,78 Short Lane,29384857\n
+										Clive,Smith,49 Sutherland St,31214788\n
+										James,Owen,8 Crimson Rd,32114566\n
+										Graham,Brown,94 Roland St,8766556\n";
+
+		//ENSURE ARRAY EXTENSION GET METHOD RETURNS EMPTY IF NULL OR KEY NOT FOUND
+		[TestMethod]
+		public void TestArrayExtensionReturnsEmptyIfKeyNotFoundOrParameterNull()
+		{
+			string[][] candidate = null;
+			Assert.IsTrue(SimpleSolution.TwoDStringArrayExtensions.Get(candidate, "asdasd").Length == 0);
+
+			candidate = new string[][] { new string[1] { "MyKey" } };
+			Assert.IsTrue(SimpleSolution.TwoDStringArrayExtensions.Get(candidate, "asdasd").Length == 0);
+		}
+
+		//ENSURE ARRAY EXTENSION GET METHOD RETURNS RESULT IF KEY FOUND
+		[TestMethod]
+		public void TestArrayExtensionReturnsResultWhenKeyFound()
+		{
+			string[][] candidate = null;
+			
+			candidate = new string[][] { new string[2] { "MyKey", "SomeData" } };
+			
+			Assert.IsTrue(SimpleSolution.TwoDStringArrayExtensions.Get(candidate, "MyKey").Length >0);
+		}
+
+
+		//ENSURE LoadCSVFile RETURNS FALSE ON ATTEMPT LOAD INVALID DATA
+		[TestMethod]
+		public void TestLoadCSVFileReturnsFalseWhenInvalid()
+		{
+			byte[] data = null;
+			string[][] output = null;
+			Assert.IsFalse(SimpleSolution.Program.LoadCSVFile(data, out output));
+
+			string someInputString = "Some Non Comma Delimited Text";
+			data = System.Text.Encoding.ASCII.GetBytes(someInputString);
+			Assert.IsFalse(SimpleSolution.Program.LoadCSVFile(data, out output));
+
+			someInputString = "Some Comma, Separated\nText Without All Lines\nBeing Comma, Separated";
+			data = System.Text.Encoding.ASCII.GetBytes(someInputString);
+			Assert.IsFalse(SimpleSolution.Program.LoadCSVFile(data, out output));
+		}
+
+		//ENSURE LoadCSVFile RETURNS TRUE AND RESULTS HAVE VALUE ON ATTEMPT LOAD VALID DATA
+		[TestMethod]
+		public void TestLoadCSVFileReturnsTrueAndOutPutHasValueWhenValid()
+		{
+			byte[] data = null;
+			string[][] output = null;
+			string someInputString = testData;
+			data = System.Text.Encoding.ASCII.GetBytes(someInputString);
+
+			Assert.IsTrue(SimpleSolution.Program.LoadCSVFile(data, out output));
+			Assert.IsTrue(output != null);
+			Assert.IsTrue(output.Length > 0);
+			Assert.IsTrue(output[0].Length > 0);
+		}
+
+		//ENSURE COLUMN TESTING METHOD RETURNS FALSE WHEN NOT ALL INPUTS MATCH DATA
+		[TestMethod]
+		public void TestContainsColumnsReturnsFalseWhenInValid()
+		{
+			byte[] data = System.Text.Encoding.ASCII.GetBytes(testData);
+			string[][] output = null;
+			SimpleSolution.Program.LoadCSVFile(data, out output);
+			
+			Assert.IsFalse(SimpleSolution.Program.ContainsColumns(output, "SomeNonExistantColumn"));
+			Assert.IsFalse(SimpleSolution.Program.ContainsColumns(output, "FirstName", "SomeNonExistantColumn"));
+		}
+
+		//ENSURE COLUMN TESTING METHOD RETURNS TRUE WHEN ALL INPUTS MATCH DATA
+		[TestMethod]
+		public void TestContainsColumnsReturnsTrueWhenValid()
+		{
+			byte[] data = System.Text.Encoding.ASCII.GetBytes(testData);
+			string[][] output = null;
+			SimpleSolution.Program.LoadCSVFile(data, out output);
+			Assert.IsTrue(SimpleSolution.Program.ContainsColumns(output, "FirstName"));
+			Assert.IsTrue(SimpleSolution.Program.ContainsColumns(output, "FirstName", "LastName"));
+		}
+	}
+}
---
+++ SimpleSolution/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimpleSolution
+{
+	public static class TwoDStringArrayExtensions
+	{
+		//EXTENSION METHOD TO EASILY ACCESS DATA BY KEY
+		public static string[] Get(this string[][] array, string key)
+		{
+			var keyData = (array == null || array.Length == 0) ? Array.Empty<string>() : array.FirstOrDefault(x => x[0] == key);
+			return keyData == null ? Array.Empty<string>() : keyData.Skip(1).ToArray();
+		}
+	}
+
+	//CLASS MADE PUBLIC FOR TESTABILITY
+	public class Program
+	{
+		//METHOD MADE PUBLIC FOR TESTABILITY
+		public static void Main(string[] args)
+		{
+			//LOAD DATA INTO READABLE FORMAT ONCE
+			string[][] data = null;
+
+			//ENSURE DATA IS LOADABLE AS REQUIRED
+			if (!LoadCSVFile(Properties.Resources.data, out data)) return;
+
+			//ENSURE DATA IS IN A FORMAT SUITABLE FOR THIS EXAMPLE
+			if (!ContainsColumns(data, "FirstName", "LastName")) return;
+
+			//SET FILENAMES
+			string nameDataFileName = "NameData.txt";
+			string addressDataFileName = "AddressData.txt";
+
+			//EXTRACT NAME DATA ORDERED BY FREQUENCY THEN ALPHABETICALLY
+			ExtractNameData(data, nameDataFileName);
+
+			//EXTRACT ADDRESS DATA ORDERED ALPHABETICALLY
+			ExtractAddressData(data, addressDataFileName);
+
+			//OPEN FILES FOR DISPLAY PURPOSES
+			System.Diagnostics.Process.Start(nameDataFileName);
+			System.Diagnostics.Process.Start(addressDataFileName);
+		}
+
+		//A METHOD TO CONFIRM INPUT DATA HOLDS WHAT IS IN INTEREST
+		public static bool ContainsColumns(string[][] data, params string[] columns)
+		{
+			return (columns != null && columns.All(x => data.Get(x).Length > 0));
+		}
+
+		//METHOD MADE PUBLIC FOR TESTABILITY
+		public static void ExtractNameData(string[][] data, string fileName)
+		{
+			//GET ARRAY OF FIRSTNAMES AND LASTNAMES
+			var values = data.Get("FirstName").Concat(data.Get("LastName"));
+
+			//CREATE A STRING BUILDER FOR EASE OF STRING BUILDING AND MEMORY CONSUMPTION
+			StringBuilder builder = new StringBuilder();
+
+			//CYCLE THROUGH STRING LIST, GROUP, COUNT AND ORDER AND SELECT DESIRABLE OBJECT TYPE
+			foreach (var result in (from val in values
+									group val by val into g
+									select new { Value = g.Key, Frequency = g.Count() }).OrderByDescending(x => x.Frequency).ThenBy(x => x.Value))
+			{
+				//APPEND LINE USING DESIRABLE OBJECT TYPE
+				builder.AppendLine(string.Format("{0}, {1}", result.Value, result.Frequency));
+			}
+
+			//WRITE RESULTS TO FILE
+			System.IO.File.WriteAllText(fileName, builder.ToString());
+		}
+
+		//METHOD MADE PUBLIC FOR TESTABILITY
+		public static void ExtractAddressData(string[][] data, string fileName)
+		{
+			//GET ARRAY OF ADDRESSES
+			var values = data.Get("Address");
+
+			//CREATE A STRING BUILDER FOR EASE OF STRING BUILDING AND MEMORY CONSUMPTION
+			StringBuilder builder = new StringBuilder();
+
+			//CYCLE THROUGH STRING LIST, ORDER USING FIRST ALPHABETIC WORD AND SELECT RESULTS
+			foreach (var result in (from val in values
+									orderby Regex.Match(val, "[A-Za-z]+").Value
+									select val))
+			{
+				//APPEND LINE USING ITEREE
+				builder.AppendLine(result);
+			}
+
+			//WRITE RESULTS TO FILE
+			System.IO.File.WriteAllText(fileName, builder.ToString());
+		}
+
+		//LOAD THE DATA 
+		//ASSUMING DATA IS NOT EMPTY
+		//USE LIGHTWEIGHT RETURN TYPE AS VESSEL
+		//METHOD MADE PUBLIC FOR TESTABILITY
+		public static bool LoadCSVFile(byte[] bytes, out string[][] results)
+		{
+			//ENSURE THERE IS DATA TO BE READ
+			bool isValid = bytes != null && bytes.Length > 0;
+
+			//ASSIGN DEFAULT OUTPUT
+			results = new string[][] { };
+
+			//DATA IS NOT NULL OR EMPTY
+			if (isValid)
+			{
+				//PREPARE A LIST FOR EXTRACTED LINES
+				List<string> lines = new List<string>();
+
+				using (System.IO.MemoryStream ms = new System.IO.MemoryStream(bytes))
+				{
+					using (System.IO.TextReader reader = new System.IO.StreamReader(ms))
+					{
+						//INITIATE THE READ PROCESS
+						string header = reader.ReadLine();
+						for (string line = header; !string.IsNullOrEmpty(line); line = reader.ReadLine())
+						{
+							//APPEND THE REST OF THE LINES
+							lines.Add(line);
+						}
+					}
+					//DONE WITH READER
+				}
+				//DONE WITH MEMORY STREAM
+
+				//ENSURE READABLE DATA (HAS A HEADER ROW + MORE)
+				isValid = lines.Count > 1;
+
+				//DATA IS MADE UP OF LINES
+				if (isValid)
+				{
+					//ENSURE DATA IS USABLE
+					isValid = lines.All(x => x.IndexOf(',') > -1);
+
+					//DATA IS IN CSV FORMAT USING COMMA AS DELIMETER
+					if (isValid)
+					{
+						//GET THE HEADER LINE
+						string headerLine = lines.First();
+
+						//GET THE COLUMNS INTO A LIST FOR MINIMIZED OVERHEAD OF INDEXOF
+						List<string> headerColumns = headerLine.Split(',').ToList();
+
+						//GET ACTUAL DATA
+						IEnumerable<string> data = lines.Skip(1);
+
+						//RETURN DATA
+						results = (from header in headerColumns
+								   let headerIndex = headerColumns.IndexOf(header)
+								   select new string[1] { header }.Concat(data.Select(x => x.Split(',')[headerIndex])).ToArray()).ToArray();
+					}
+				}
+			}
+			return isValid;
+		}
+	}
+}
